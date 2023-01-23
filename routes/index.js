6524be533395a4110256398e6723735274bb2381
Richard Lasco <lasco9@gmail.com>
2023-01-23 16:22:23 -0600

    Added route handling for new message page

--- routes/index.js
+++ routes/index.js
@@ -20,4 +20,22 @@ router.get('/', function (req, res, next) {
   res.render('index', { title: 'Mini Messageboard', messages: messages });
 });
 
+/* Get new message page */
+router.get('/new', function (req, res, next) {
+  res.render('new');
+});
+
+router.post('/new', function (req, res, next) {
+  const user = req.body.user;
+  const messageText = req.body.messageText;
+
+  messages.push({
+    user: user,
+    text: messageText,
+    added: moment().format('MMMM Do YYYY, h:mm a'),
+  });
+
+  res.redirect('/');
+});
+
 module.exports = router;
