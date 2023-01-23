1a13b582173049f15fe67182e6a71275c70d1b62
Richard Lasco <lasco9@gmail.com>
2023-01-23 16:03:08 -0600

    Added messages object to index route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
 var express = require('express');
+var moment = require('moment');
 var router = express.Router();
 
+const messages = [
+  {
+    text: 'Hi there!',
+    user: 'Amando',
+    added: moment().format('MMMM Do YYYY, h:mm a'),
+  },
+  {
+    text: 'Hello World!',
+    user: 'Charles',
+    added: moment().format('MMMM Do YYYY, h:mm a'),
+  },
+];
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Mini Messageboard', messages: messages });
 });
 
 module.exports = router;
